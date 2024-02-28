c76209f8a1bf2c249b2f1e0383e17516435a0a82
30173875 <30173875@nescol.ac.uk>
2024-02-28 09:03:44 +0000

    .

---
+++ Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame Main_page = new JFrame("Calculator");
+        JPanel Main_panel = new JPanel();
+
+        Main_page.setSize(800,800);
+        Main_page.add(Main_panel);
+        Main_page.setVisible(true);
+    }
+}
